725721adc92cf64119d2429d8f0f359e27352504
alberto souza <alots.ssa@gmail.com>
2020-10-08 15:02:28 -0300

    comeco

---
+++ src/main/java/br/com/zup/bootcamp/bolao/DesafioBolaoBootcampApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.bootcamp.bolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBolaoBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBolaoBootcampApplication.class, args);
+	}
+
+}
