86ed05642031b9fb4234ccfa8cb6bf0a1e8238c4
Rui Braz <rui.braz.correia@gmail.com>
2020-10-13 15:39:44 -0300

    Tratativa inicial duplicidade de email.

--- src/main/java/br/com/zup/bootcamp/bolao/advice/HandlerErro.java
+++ src/main/java/br/com/zup/bootcamp/bolao/advice/HandlerErro.java
@@ -1,6 +1,21 @@
 package br.com.zup.bootcamp.bolao.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class HandlerErro {
 
-    
+    private final Logger log = LoggerFactory.getLogger(HandlerErro.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handle (MethodArgumentNotValidException methodArgumentNotValidException){
+        log.info("Tratando {}", methodArgumentNotValidException);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new String ("Teste do Luram"));
+    }
 }
